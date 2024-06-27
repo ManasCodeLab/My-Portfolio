03bf7605729c68cd0c8ee8736189f1e5bb4fb057
ᗰᗩᑎᗩᔕ ᗩᖇOᖇᗩ <aroramanas01@gmail.com>
2024-06-27 13:04:49 +0530

    update

---
+++ public/bd/js/index.js
@@ -0,0 +1,36 @@
+const count = document.getElementById('count');
+const head = document.getElementById('head');
+const giftbox = document.getElementById('merrywrap');
+const canvasC = document.getElementById('c');
+
+const config = {
+  birthdate: 'Aug 15, 2023',
+  name: "Momo 🤍"
+};
+
+const confettiSettings = { target: 'confetti' };
+const confetti = new window.ConfettiGenerator(confettiSettings);
+confetti.render();
+
+const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+
+let countDown = new Date(`${config.birthdate} 00:00:00`).getTime();
+x = setInterval(function() {
+  let now = new Date().getTime(),
+    distance = countDown - now;
+
+  document.getElementById('day').innerText = Math.floor(distance / day);
+  document.getElementById('hour').innerText = Math.floor(
+    (distance % day) / hour
+  );
+  document.getElementById('minute').innerText = Math.floor(
+    (distance % hour) / minute
+  );
+  document.getElementById('second').innerText = Math.floor(
+    (distance % minute) / second
+  );
+
+  })
